09831db24b33fb97e084bb3181179e2e61713784
Antoine <Antoine.DELUBRIAC@stagiaire-elan.fr>
2024-02-28 15:57:21 +0100

    Exercice 6 fin

---
+++ exo6.php
@@ -0,0 +1,52 @@
+<h1>Exercice 6</h1>
+
+<p>
+Créer une fonction personnalisée permettant de remplir une liste déroulante à partir d'un tableau 
+de valeurs.
+Exemple :
+$elements = array("Monsieur","Madame","Mademoiselle");
+alimenterListeDeroulante($elements);
+</p>
+
+<h2>Résultat</h2>
+
+<?php
+
+// Création d'un tableau numérique avec les donnés à affciher : 
+$tableauCivilite = [
+    "Madame",
+    "Monsieur",
+    "Mademoiselle"
+];
+
+// Structure du menu :
+echo
+"<label for='Civilité'>Civilité</label>
+
+<select name='Civilité' id='Civilité'>
+    <option value=>--Choisir une option--</option>";
+// Création d'une fonction qui va ajouter les choix au menu déroulant : 
+function menuDeroulant($choix) {
+foreach($choix as $cle) {
+    echo "
+    <option value='$cle'>$cle</option>";
+}
+}
+"</select>";
+
+// Afficher le menu déroulant : 
+menuDeroulant($tableauCivilite);
+
+
+// Création du menu déroulant html :
+?>
+
+<!-- <label for='Civilité'>Civilité</label>
+
+<select name='Civilité' id='Civilité'>
+    <option value="">--Choisir une option--</option>
+
+        <option value='Monsieur'>Monsieur</option>
+        <option value='Madame'>Madame</option>
+        <option value='Mademoiselle'>Mademoiselle</option>
+</select> -->
