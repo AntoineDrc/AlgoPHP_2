6b34a97f7374de2a0bdddcd1bd89d8b7f3671919
Antoine <Antoine.DELUBRIAC@stagiaire-elan.fr>
2024-02-28 11:03:40 +0100

    Exercice 3 fin

---
+++ exo3.php
@@ -0,0 +1,11 @@
+<h1>Exercice 3</h1>
+
+<p>
+Afficher un lien hypertexte permettant d'accéder au site d'Elan Formation. Le lien devra s'ouvrir 
+dans un nouvel onglet (_blank)
+</p>
+
+<h2>Résultat</h2>
+
+<!-- Lien pour le site Elan Formation -->
+<a href="http://elan-formation.fr/accueil" target="_blank" rel="noopener noreferrer">Site Elan Formation</a>
